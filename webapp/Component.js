1368c6cb12be446bbf8391d01f15cb0462ca2878
liuxw <liuxiaowennn@gmail.com>
2021-01-13 20:32:15 +0800

    feat: invoke the dialog at component level

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,6 +1,10 @@
 sap.ui.define(
-  ["sap/ui/core/UIComponent", "sap/ui/model/json/JSONModel"],
-  function (UIComponent, JSONModel) {
+  [
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/json/JSONModel",
+    "./controller/HelloDialog",
+  ],
+  function (UIComponent, JSONModel, HelloDialog) {
     "use strict";
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
       metadata: {
@@ -17,6 +21,18 @@ sap.ui.define(
         };
         var oModel = new JSONModel(oData);
         this.setModel(oModel);
+
+        // set dialog
+        this._helloDialog = new HelloDialog(this.getRootControl());
+      },
+
+      exit: function () {
+        this._helloDialog.destroy();
+        delete this._helloDialog;
+      },
+
+      openHelloDialog: function () {
+        this._helloDialog.open();
       },
     });
   }
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,4 +1,9 @@
 sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
   "use strict";
-  return Controller.extend("sap.ui.demo.walkthrough.controller.App", {});
+
+  return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+    onOpenDialog: function () {
+      this.getOwnerComponent().openHelloDialog();
+    },
+  });
 });
